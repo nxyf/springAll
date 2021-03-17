171e2ca6229e90f451afa4a0a4c37b47a9e76267
yunjie <yunjie_nx@163.com>
2021-03-17 11:47:17 +0800

    mybatisplus分页插件

--- springboot-mybatis-plus/src/main/java/com/nxyf/springbootmybatisplus/config/MybatisPlusConfig.java
+++ springboot-mybatis-plus/src/main/java/com/nxyf/springbootmybatisplus/config/MybatisPlusConfig.java
@@ -1,6 +1,8 @@
 package com.nxyf.springbootmybatisplus.config;
 
-import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,7 +17,9 @@ public class MybatisPlusConfig {
      * 分页插件
      */
     @Bean
-    public PaginationInterceptor paginationInterceptor() {
-        return new PaginationInterceptor();
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.H2));
+        return interceptor;
     }
 }
